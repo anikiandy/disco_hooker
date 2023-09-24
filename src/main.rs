c3096979983adaf452f7b3685738571e329dad1b
andy <ashiraki@proton.me>
2023-09-23 23:02:15 -0700

    refactor still remains functional

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 mod payload;
 use payload::Payload;
 
-
-
 fn main() {
-
     let endpoint = String::from("https://discord.com/api/bhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl");
     let user_name = String::from("andy");
     let avatar_url = String::from("default");
 
-    let mut hooker: Payload = Payload::new(endpoint, user_name, avatar_url);
+    let mut hooker: Payload = Payload::new( user_name, avatar_url);
 
     hooker.set_content(String::from("boogers"));
     hooker.set_title(String::from("its a title"));
@@ -18,6 +15,4 @@ fn main() {
     let mut hooker2 = Payload::new_from_json("settings.json");
 
     println!("hooker2: {:?}", hooker2);
-
-    
 }
--- src/payload.rs
+++ src/payload.rs
@@ -1,41 +1,50 @@
 use serde::Deserialize;
-use std::{fs, f64::consts::E};
-use serde_json::{Result, Value};
-
+use serde_json::{Result as JsonResult, Value};
+use std::fs;
 
 #[derive(Deserialize, Debug)]
 pub struct Payload {
     user_name: String,
     content: Option<String>,
     title: Option<String>,
-    avatar_url: String,
-    end_point: String,
+    avatar_url: Option<String>,
+    end_point: Option<String>,
 }
 
 impl Payload {
-    pub fn new(end_point: String, user_name: String, avatar_url: String) -> Payload {
+    pub fn new(user_name: String, avatar_url: String) -> Payload {
         Payload {
             user_name,
             content: None,
             title: None,
-            avatar_url,
-            end_point,
+            avatar_url: Some(avatar_url),
+            end_point: None,
         }
     }
-    
-    pub fn new_from_json(file_path: &str) -> Payload {
 
+    //fn to take a config file to setup the payload with the endpoint and user and user avatar
+    pub fn new_from_json(file_path: &str) -> Payload {
         let settings = fs::read_to_string(file_path).unwrap();
         let v: Value = serde_json::from_str(&settings).unwrap();
-        Payload { 
-            user_name: v["user_name"].as_str().unwrap().to_owned(), 
-            content: None, 
-            title: None, 
-            avatar_url: v["avatar_url"].as_str().unwrap().to_owned(), 
-            end_point: v["end_point"].as_str().unwrap().to_owned(), 
+        Payload {
+            //TODO: replace with unwarp() with a default behavior incase that fiels is not parsed from the json
+            user_name: v["user_name"].as_str().unwrap().to_owned(),
+            content: None,
+            title: None,
+            avatar_url: Some(v["avatar_url"].as_str().unwrap().to_owned()),
+            end_point: Some(v["end_point"].as_str().unwrap().to_owned()),
         }
-        
     }
+
+    pub fn set_endpoint(&mut self, end_point: String) -> Result<(), String> {
+        match self.end_point {
+            None => {
+                self.end_point = Some(end_point);
+                Ok(())
+            }
+            Some(_) => Err(String::from("Endpoint already set!")),
+        }
+    } // End set_endpoint
 
     pub fn set_title(&mut self, title: String) {
         self.title = Some(title);
