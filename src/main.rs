3265ee1c9e8b699b87092afadb467ab056c893f5
andy <ashiraki@proton.me>
2023-09-24 19:36:34 -0700

    reqwest function works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod payload;
 use payload::Payload;
+use tokio::main;
+use reqwest::{Client,Error};
 
-fn main() {
-    let endpoint = String::from("https://discord.com/api/bhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl");
+
+#[tokio::main]
+async fn main() -> Result<(),Error>{
+    let _endpoint = "https://discordapp.com/api/webhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl";
+                                 //        "https://discordapp.com/api/webhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl"
     let user_name = String::from("andy");
     let avatar_url = String::from("default");
 
@@ -15,4 +20,14 @@ fn main() {
     let mut hooker2 = Payload::new_from_json("settings.json");
 
     println!("hooker2: {:?}", hooker2);
+
+    let content = r#"{"content": "boogers","username": "sandy","avatar": "689161dc90ac261d00f1608694ac6bfd","application_id": "658822586720976555"}"#;
+    let client = reqwest::Client::new();
+    let res = client.post(_endpoint)
+        .header("Content-Type", "application/json")
+        .body(content.to_owned())
+        .send()
+        .await?;
+    println!("didntfail {:?} ", res);
+    Ok(())
 }
