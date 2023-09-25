bd16ec6599439eb10f5a69fa2340337a80ff10ef
andy <ashiraki@proton.me>
2023-09-24 21:06:55 -0700

    payload handler thing works

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
 mod payload;
 use payload::Payload;
+use reqwest::{Client, Error};
 use tokio::main;
-use reqwest::{Client,Error};
-
 
 #[tokio::main]
-async fn main() -> Result<(),Error>{
+async fn main(){
     let _endpoint = "https://discordapp.com/api/webhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl";
-                                 //        "https://discordapp.com/api/webhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl"
+    //        "https://discordapp.com/api/webhooks/1021825932442472488/DmLKFLggwYPT625-XvttvxA1v4Iqwk7nM0-k75J7UuBqxEqVKX44a-mbsqldgcGJE4Pl"
     let user_name = String::from("andy");
     let avatar_url = String::from("default");
 
-    let mut hooker: Payload = Payload::new( user_name, avatar_url);
+    let mut hooker: Payload = Payload::new(user_name, avatar_url);
 
     hooker.set_content(String::from("boogers"));
     hooker.set_title(String::from("its a title"));
 
     println!("{:?}", hooker);
     let mut hooker2 = Payload::new_from_json("settings.json");
+    hooker2.set_content(String::from("content hello I like boogesr"));
+    hooker2.send_msg().await.expect("FAILED TO DO THE THING");
 
     println!("hooker2: {:?}", hooker2);
 
-    let content = r#"{"content": "boogers","username": "sandy","avatar": "689161dc90ac261d00f1608694ac6bfd","application_id": "658822586720976555"}"#;
-    let client = reqwest::Client::new();
-    let res = client.post(_endpoint)
-        .header("Content-Type", "application/json")
-        .body(content.to_owned())
-        .send()
-        .await?;
-    println!("didntfail {:?} ", res);
-    Ok(())
+
 }
--- src/payload.rs
+++ src/payload.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use serde_json::{Result as JsonResult, Value};
+use serde_json::Value;
 use std::fs;
 
 #[derive(Deserialize, Debug)]
@@ -12,6 +12,7 @@ pub struct Payload {
 }
 
 impl Payload {
+    // Constructor that takes user name and avatar url
     pub fn new(user_name: String, avatar_url: String) -> Payload {
         Payload {
             user_name,
@@ -27,7 +28,7 @@ impl Payload {
         let settings = fs::read_to_string(file_path).unwrap();
         let v: Value = serde_json::from_str(&settings).unwrap();
         Payload {
-            //TODO: replace with unwarp() with a default behavior incase that fiels is not parsed from the json
+            //TODO: replace with unwrap()s with a default behavior incase that field is not parsed from the json
             user_name: v["user_name"].as_str().unwrap().to_owned(),
             content: None,
             title: None,
@@ -36,21 +37,48 @@ impl Payload {
         }
     }
 
-    pub fn set_endpoint(&mut self, end_point: String) -> Result<(), String> {
-        match self.end_point {
-            None => {
-                self.end_point = Some(end_point);
-                Ok(())
-            }
-            Some(_) => Err(String::from("Endpoint already set!")),
-        }
-    } // End set_endpoint
-
     pub fn set_title(&mut self, title: String) {
         self.title = Some(title);
     }
 
     pub fn set_content(&mut self, content: String) {
         self.content = Some(content);
+    }
+
+    //send message to discord should return an Error if there is no message
+    pub async fn send_msg(&self) -> Result<(), &str> {
+        //return err if there is no content
+        if self.content.is_none() {
+            return Err("No message content set");
+        } else if self.end_point.is_none() {
+            // Check if there is an endpoint
+            return Err("No endpoint set");
+        }
+
+        // Send
+        println!(" FORMATTED PAYLOAD: {}", self.fmt_payload());
+        let content = r#"{"content": "boogers","username": "sandy","avatar": "689161dc90ac261d00f1608694ac6bfd","application_id": "658822586720976555"}"#;
+
+        let client = reqwest::Client::new();
+        let res = client
+            .post(self.end_point.as_ref().unwrap()) //safe to unwrap because checked already
+            .header("Content-Type", "application/json")
+            .body(self.fmt_payload())
+            .send()
+            .await;
+        if res.is_err() {
+            return Err("Recieved request error");
+        }
+
+        Ok(())
+    } // send_msg fn
+
+    fn fmt_payload(&self) -> String {
+        let content = self.content.as_ref().unwrap();
+        let avatar = self.avatar_url.as_ref().unwrap();
+        format!(
+            r#"{{"content":"{}", "username": "{}", "avatar": "{}"}}"#,
+            content, self.user_name, avatar
+        )
     }
 }
